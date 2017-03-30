e9a641f1f67ced8c06094c7de34ad3f412507c90
Raymond Aung <raymond.psaung@gmail.com>
2017-03-30 15:38:41 +0800

    cleaned up code and added comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,48 @@
+/**
+ * NOTE: To make sure Game lib script (Othello.js) is preloaded before this script
+ */
+
 document.addEventListener('DOMContentLoaded', function () {
   // code and events go here
   var grid = document.querySelector('.grid')
   var img
   var game = new Othello()
 
+  /**
+   * Restart button to refresh the page.
+   */
   document.querySelector('.myButton').addEventListener('click', function () {
     window.location.reload(true)
   })
 
+  /**
+   * Call the game api and create the initial board in UI
+   */
   game.initiateGame()
   createBoard()
 
   function createBoard () {
+    /**
+     * Create each tile as div box
+     * setting id and default position of the pieces
+     * 4 pieces at the center of the board prepopulated with 2 pieces each for black and white
+     */
     var boxDiv
     for (var i = 0; i < game.tiles.length; i++) {
       for (var j = 0; j < game.tiles[i].length; j++) {
         boxDiv = document.createElement('div')
         boxDiv.id = 'box_' + i + '_' + j
         boxDiv.classList.add('box')
-        if ((j + i) % 2) {
-          boxDiv.classList.add('boxColor1')
-        } else {
-          boxDiv.classList.add('boxColor2')
-        }
         if (game.tiles[ i ][ j ] !== null) {
           img = document.createElement('img')
           img.src = 'assets/images/' + game.tiles[ i ][ j ].toLowerCase() + '.png'
           img.classList.add('filledBox')
           boxDiv.appendChild(img)
         }
+
+        // add Event listeners
+        // 1. when the user clicks, send the moveIndex to game controller.
+        // 2/3. when the user hovers over the empty space, it tells the user whether user can make the move in that tile
         boxDiv.addEventListener('click', processClick)
         boxDiv.addEventListener('mouseenter', showSilhouette)
         boxDiv.addEventListener('mouseleave', removeSilhouette)
@@ -38,56 +52,64 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function processClick () {
-    // alert(this.id.split('_')[1] + ',' + this.id.split('_')[2])
-    // check for all the avaialble moves and if it's in there show the hover effect
-    // var moveIndex = [ parseInt(this.id.split('_')[ 1 ]), parseInt(this.id.split('_')[ 2 ]) ]
-    removeSilhouette.call(this)
+    /**
+     * 1. Checks if the move is valid.
+     * 2. if yes, reverse the tiles/ flipped white to black, vice versa
+     * 3. Switch the turn after the valid move.
+     * 4. Auto-Switch the turn if there is no available move for the next player.
+     * 5. Display the updated score report in UI
+     * 6. Check for game over condition, if yes, display the winner in UI
+     */
+    removeSilhouette.call(this) // remove the silhouette first
+
+    // only if the tile is empty, go ahead with the logic
+    // TODO: refactor the game api to not rerun the same code
     if (!this.querySelector('img.filledBox')) {
-      // console.log('can move')
       var moveIndex = [ parseInt(this.id.split('_')[ 1 ]), parseInt(this.id.split('_')[ 2 ]) ]
       var directions
-      // var currentPlayer = game.currentPlayer()
       if (game.isValidMove(moveIndex)) {
         directions = game.getValidDirections(moveIndex)
         directions.forEach(function (direction) {
           var toIndex = game.checkIdenticalTileExists(moveIndex, direction)
           if (toIndex) {
-            // console.log('reverse tiles from ' + moveIndex + ' to ' + toIndex + '')
-            // setImage.call(this)
+            // in UI, flip the tiles
             reverseDivBoxes(moveIndex, toIndex, direction)
+            // reverse the tiles in game object.
             game.reverseTiles(moveIndex, toIndex, direction)
           }
         })
+
+        // switch the turn, update UI's turn indicator
         game.switchTurn()
         switchPlayerDisplay()
+
+        // update score report.
         updateScore(game.getScoreArray())
+
         // if there's no more available moves for next player, auto switch turn.
-        // if game's over, show who's winner
+        // if game's over, show who's winner, When it called the game
         if (!game.availableMoves().length) {
           if (game.isGameOver()) {
             // all board filled or no moves available for both players
             var newScore = game.getScoreArray()
             var winner = game.whoWon() === PLAYER_1 ? 'player1' : 'player2'
             var loser = winner === 'player1' ? 'player2' : 'player1'
+            // if all times in the game board have been filled
             if (game.getFlattenedTilesArray().length === 64) {
               updateScore(newScore)
-              // alert(winner + ' Wins! Score: ' + newScore[ 0 ] + ' - ' + newScore[ 1 ])
               callSweetAlert(winner.toUpperCase() + ' Wins! Score: ' + newScore[ 0 ] + ' - ' + newScore[ 1 ])
             } else {
+              // the game board has not been fully filled, but no more available moves for both players, the game will end with the current score.
               updateScore(newScore)
-              // alert('No More available Moves. ' + winner + ' Wins! Score: ' + newScore[ 0 ] + ' - ' + newScore[ 1 ])
               callSweetAlert('No More available Moves. ' + winner.toUpperCase() + ' Wins! Score: ' + newScore[ 0 ] + ' - ' + newScore[ 1 ])
-              // document.querySelector('div.pointer.' + winner + ' img').src = 'assets/images/winner.png'
-              // document.querySelector('div.pointer.' + winner + ' img').style = 'block'
             }
-            // document.querySelector('div.pointer.' + winner + ' img').querySelector('img').src = 'assets/images/winner.png'
+            // Update in UI to indicate winner.
             document.querySelector('div.pointer.' + winner + ' img').src = 'assets/images/winner.png'
             document.querySelector('div.pointer.' + winner).style.display = 'block'
             document.querySelector('div.pointer.' + loser).style.display = 'none'
           } else {
-            // turn was auto switched due to no avilable moves
-            // auto-turning was triggerred when it checked for isGameOver()
-            // alert('No more available moves for ' + game.nextPlayer() + '. Turn was auto-switched.')
+            // When isGameOver() was called, turn was auto-switched, Update Turn in UI needs to be called again to be in sync with the game progress.
+            switchPlayerDisplay()
             callSweetAlert('No more available moves for ' + game.nextPlayer() + '. Turn was auto-switched.')
           }
         }
@@ -96,10 +118,13 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function showSilhouette () {
+    /**
+     * show silhouette when the user hovers over the empty tile and it is a valid move.
+     */
     if (!this.querySelector('img') || !this.querySelector('img.dimmy')) {
+      // box id is in format 'box_x_y'/'box_1_1'
       var moveIndex = [ parseInt(this.id.split('_')[ 1 ]), parseInt(this.id.split('_')[ 2 ]) ]
       if (game.isValidMove(moveIndex)) {
-        // console.log(moveIndex + 'is a valid move.')
         img = document.createElement('img')
         img.classList.add('dimmy')
         img.src = 'assets/images/' + game.currentPlayer().toLowerCase() + '.png'
@@ -109,33 +134,38 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function removeSilhouette () {
+    /**
+     * remove the silhouette when the user moves out from the empty cell
+     */
     if (this.querySelector('img.dimmy')) {
       this.removeChild(this.querySelector('img.dimmy'))
     }
   }
 
   function reverseDivBoxes (fromIndex, toIndex, direction) {
+    /**
+     *  UI update: flip all the box divs from the fromIndex to toIndex in the given direction.
+     */
     var xIncrement = DIRECTIONS_MOVE_MAP[ direction ][ 'x' ]
     var yIncrement = DIRECTIONS_MOVE_MAP[ direction ][ 'y' ]
     var newX = fromIndex[ 0 ]
     var newY = fromIndex[ 1 ]
     var toX = toIndex[ 0 ]
     var toY = toIndex[ 1 ]
-    // console.log(newX, newY)
-    // console.log(toX, toY)
     while (newX !== toX || newY !== toY) {
       if (game.tiles[ newX ][ newY ] !== game.currentPlayer()) {
-        setImage.call(document.querySelector('#box_' + newX + '_' + newY))
-        // game.tiles[ newX ][ newY ] = game.currentPlayer()
-        // console.log('change (' + newX + ',' + newY + ') to ' + this.playerTurn)
+        flipImage.call(document.querySelector('#box_' + newX + '_' + newY))
       }
       newX += xIncrement
       newY += yIncrement
     }
   }
 
-  function setImage () {
-    // console.log(this)
+  function flipImage () {
+    /**
+     *  changing the image from black to white for existing tiles, vice versa
+     *  for empty tile, will create a new image with the current player colour
+     */
     if (this.querySelector('img.filledBox')) {
       var imageToflip = this.querySelector('img.filledBox')
       if (imageToflip.src.split('/').reverse()[0] === 'white.png') {
@@ -144,9 +174,7 @@ document.addEventListener('DOMContentLoaded', function () {
         imageToflip.src = 'assets/images/white.png'
       }
     } else {
-      // console.log('is blank')
       img = document.createElement('img')
-      // img.src = 'assets/images/black.png'
       img.src = 'assets/images/' + game.currentPlayer().toLowerCase() + '.png'
       img.classList.add('filledBox')
       this.appendChild(img)
@@ -154,6 +182,9 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function switchPlayerDisplay () {
+    /**
+     * toggle display between the player pointer
+     */
     if (game.currentPlayer() === PLAYER_1) {
       document.querySelector('div.pointer.player2').style.display = 'none'
       document.querySelector('div.pointer.player1').style.display = 'block'
@@ -164,10 +195,17 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function updateScore (newScore) {
+    /**
+     * Update the score on UI
+     */
     document.querySelector('.score').textContent = newScore[ 0 ] + ' - ' + newScore[ 1 ]
   }
 
   function callSweetAlert (aMessage) {
+    /**
+     * Helper alert function (SweetAlert2 Api) to display cool messages.
+     * <script src="https://cdn.jsdelivr.net/sweetalert2/6.4.4/sweetalert2.min.js"></script>
+     */
     swal({
       text: aMessage,
       timer: 5000
